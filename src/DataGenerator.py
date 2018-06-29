229de8df26e6437272a3109fec5d195535bd8c1f
Ludwig Kuznia <ludwig.c.kuznia@disney.com>
2018-06-29 14:49:00 -0400

    Removed name from DataGenerator class

--- src/DataGenerator.py
+++ src/DataGenerator.py
@@ -20,39 +20,35 @@ class DataGenerator(object):
         self.train_cases = defaultdict()
         self.max_bin_magnitude = None
 
-    def generate_x(self, instances, length, max_val, name):
-        self.train_cases[name] = defaultdict()
-        self.train_cases[name]['x'] = np.random.randint(0, max_val, size=(instances, length))
+    def generate_x(self, instances, length, max_val):
+        self.train_cases['x'] = np.random.randint(0, max_val, size=(instances, length))
 
-    def binarize_x(self, name):
-        if name not in self.train_cases.keys():
-            print('Please create X matrix for {} with the generate_x method'.format(name))
+    def binarize_x(self):
+        if 'x' not in self.train_cases.keys():
+            print('Please create X matrix for {} with the generate_x method')
         else:
-            vect_binarize = np.vectorize(binarize, otypes=[np.ndarray], )
-            self.train_cases[name]['binary_x'] = vect_binarize(self.train_cases[name]['x'])
+            vect_binarize = np.vectorize(binarize, otypes=[np.ndarray])
+            self.train_cases['binary_x'] = vect_binarize(self.train_cases['x'], self.max_bin_magnitude)
 
-    def generate_y(self, name):
-        if name not in self.train_cases.keys():
-            print('Please create X matrix for {} with the generate_x method'.format(name))
+    def generate_y(self):
+        if 'x' not in self.train_cases.keys():
+            print('Please create X matrix with the generate_x method')
         else:
-            self.train_cases[name]['y'] = np.argsort(self.train_cases[name]['x'])
+            self.train_cases['y'] = np.argsort(self.train_cases['x'])
 
-    def generate_case(self, instances, length, max_val, name):
+    def generate_case(self, instances, length, max_val):
         self.max_bin_magnitude = len(format(max_val, 'b'))
-        self.generate_x(instances, length, max_val, name)
-        self.generate_y(name)
+        self.generate_x(instances, length, max_val)
+        self.generate_y()
 
 
 data = DataGenerator()
 MAX_PWR = 5
 LIST_LEN = 5
-data.generate_case(10, LIST_LEN, 2**MAX_PWR + 1, 'test')
-data.binarize_x('test')
-rd = data.train_cases['test']['x']
-b_rd = data.train_cases['test']['binary_x']
-
-tt = vect_binarize(data.train_cases['test']['x'], 5)
+data.generate_case(10, LIST_LEN, 2**MAX_PWR + 1)
+data.binarize_x()
+rd = data.train_cases['x']
+b_rd = data.train_cases['binary_x']
 
 print(rd)
-print(t_rd)
-print(tt)
+print(b_rd)
