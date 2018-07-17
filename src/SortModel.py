0a7c1b2fb02d2369e3564426499e3b6ddfe6e9f3
Ludwig Kuznia <ludwig.c.kuznia@disney.com>
2018-07-17 16:37:56 -0400

    Introducing Tensorflow into project.

--- src/SortModel.py
+++ src/SortModel.py
@@ -1,13 +1,21 @@
 
 import src.DataGenerator as dg
+import tensorflow as tf
+from math import factorial
 
-MAX_PWR = 5
-LIST_LEN = 5
-data = dg.DataGenerator(10, LIST_LEN, 2**MAX_PWR + 1)
+MAX_PWR = 5  # This is the maximum value the elements in the vector can take
+LIST_LEN = 3  # This is the length of the vector to be sorted
+NB_INSTANCES = 100  # This is the number of training instances
+
+NB_HIDDEN = 1000  # number of hidden units
+
+data = dg.DataGenerator(NB_INSTANCES, LIST_LEN, 2**MAX_PWR + 1)
 data.generate_case()
-# data.binarize_x()
-rd = data.train_cases['x']
-b_rd = data.train_cases['binary_x']
 
-print(rd)
-print(b_rd)
+train_x = data.train_cases['x']
+train_y = data.train_cases['y']
+train_x_bin = data.train_cases['binary_x']
+train_y_bin = data.train_cases['binary_y']
+
+# X = tf.placeholder("float", [None, ])
+# Y = tf.placeholder("float", [None, factorial(LIST_LEN)])
